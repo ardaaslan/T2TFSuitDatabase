c24e3d2d6072081a839b9a5cd7e340b99007f728
onrylmz <onur.yilmzz@gmail.com>
2014-07-17 16:57:32 +0300

    Login Page is updated.

--- src/main/java/com/mycompany/login/MainPageServlet.java
+++ src/main/java/com/mycompany/login/MainPageServlet.java
@@ -36,7 +36,7 @@ public class MainPageServlet extends HttpServlet {
         }
     }*/
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold>
     /**
      * Handles the HTTP <code>GET</code> method.
      *
